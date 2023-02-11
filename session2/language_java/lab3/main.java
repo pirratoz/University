6e4978c70854d6b7981041a3bd1fe9093f5c11c4
PirraToZ <pirratoz@gmail.com>
2023-02-11 22:04:26 +0300

    add lab3 java

---
+++ session2/language_java/lab3/main.java
@@ -0,0 +1,27 @@
+package session2.language_java.lab3;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        // String str1 = scanner.nextLine();
+        String str1 = "Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства";
+        Integer count_symbol = scanner.nextInt();
+        String str2 = "бвгджзйклмнпрстфхцчшщ";
+        String str3 = "";
+        String[] words = str1.split(" ");
+        Integer count_remove = 0;
+        for (String word : words){
+            if (word.length() == count_symbol && str2.indexOf(word.toLowerCase().charAt(0)) >= 0) {
+                count_remove += 1;
+                continue;
+            }
+            str3 = String.join(" ", str3, word);
+        }
+        Double y = Math.pow(str1.length() / Math.pow(count_remove, 2), 0.5);
+        System.out.println(String.format("Количество удалённых слов: %d", count_remove));
+        System.out.println(String.format("Новая строка: %s", str3));
+        System.out.println(String.format("Y=%fl", y));
+        scanner.close();
+    }
+}
