b3459a9d4fdd6425689126e82b3d2b6840097612
PirraToZ <pirratoz@gmail.com>
2023-02-11 20:05:16 +0300

    add lab1 JAVA

---
+++ session2/language_java/lab1/main.java
@@ -0,0 +1,9 @@
+package session2.language_java.lab1;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("Привет мир!");
+        String group = "Трушин Павел 22ИП1Б(2)";
+        System.out.println(group);
+    }
+}
